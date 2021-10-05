eff1417d3462a3195b031c75458431f675950bf1
gus <gustavo_9_rj@hotmail.com>
2021-10-05 03:48:57 +0200

    video añadido, mirar como poner de bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         'supermarket2V': "url('/src/img/marketv-2.jpeg')",
         'supermarket3V': "url('/src/img/marketv-3.jpeg')",
         'supermarket4V': "url('/src/img/marketv-4.jpeg')",
+        'supermarketVideo': "url('/src/video/marketVideo-1.mp4')"
       }
     },
   },
