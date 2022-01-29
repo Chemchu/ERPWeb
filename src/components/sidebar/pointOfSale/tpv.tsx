edd097dd061badeebb2acfefe03efdf40ce8ed0f
gus <gustavo_9_rj@hotmail.com>
2022-01-29 18:13:15 +0100

    Safe commit

--- src/components/sidebar/pointOfSale/tpv.tsx
+++ src/components/sidebar/pointOfSale/tpv.tsx
@@ -66,7 +66,7 @@ const TPV = (props: { productos: Producto[], clientes: Cliente[], serverOperativ
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                                 </svg>
                             </div>
-                            <input onChange={(e) => { Filtrar(e.target.value); }} value={Busqueda} autoFocus={true} className="bg-white rounded-3xl shadow text-lg full w-full h-16 py-4 pl-16 transition-shadow focus:shadow-2xl focus:outline-none" placeholder="Buscar producto o código de barras..." />
+                            <input onChange={(e) => { Filtrar(e.target.value); }} value={Busqueda} onInputCapture={() => { console.log("Ajustar con el lector") }} autoFocus={true} className="bg-white rounded-3xl shadow text-lg full w-full h-16 py-4 pl-16 transition-shadow focus:shadow-2xl focus:outline-none" placeholder="Buscar producto o código de barras..." />
                         </div>
 
                         {/* Genera los botones de favorito */}
