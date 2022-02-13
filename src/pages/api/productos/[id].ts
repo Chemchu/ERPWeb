728a233d2e6664afb547b69b637006e54b84b937
gus <gustavo_9_rj@hotmail.com>
2022-02-13 16:54:06 +0100

    safe commit

--- src/pages/api/productos/[id].ts
+++ src/pages/api/productos/[id].ts
@@ -3,6 +3,40 @@ import { NextApiRequest, NextApiResponse } from "next"
 import GQLFetcher from "../../../utils/serverFetcher";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const ADD_NEW_PRODUCT = gql`
+        mutation AddProducto($nombre: String!, $precioVenta: Float!, $ean: String!, $proveedor: String, $familia: String, $precioCompra: Float, $iva: Float, $margen: Float, $promociones: [String], $cantidad: Int, $cantidadRestock: Int, $alta: Boolean) {
+                        addProducto(nombre: $nombre, precioVenta: $precioVenta, ean: $ean, proveedor: $proveedor, familia: $familia, precioCompra: $precioCompra, iva: $iva, margen: $margen, promociones: $promociones, cantidad: $cantidad, cantidadRestock: $cantidadRestock, alta: $alta) {
+                            message
+                            successful
+                        }
+                    }
+        `;
+
+    const AddProduct = (product: {
+        nombre: string, pVenta: number, ean: string, proveedor: string, familia: string,
+        pCompra: number, iva: number, margen: number, promociones: string, cantidad: number, cantidadRestock: number, alta: boolean
+    }) => {
+        const [createProduct] = useMutation(ADD_NEW_PRODUCT,
+            {
+                variables: {
+                    "nombre": product.nombre,
+                    "precioVenta": product.pVenta,
+                    "ean": product.ean,
+                    "proveedor": product.proveedor,
+                    "familia": product.familia,
+                    "precioCompra": product.pCompra,
+                    "iva": product.iva,
+                    "margen": product.margen,
+                    "promociones": product.promociones,
+                    "cantidad": product.cantidad,
+                    "cantidadRestock": product.cantidadRestock,
+                    "alta": product.alta
+                }
+            });
+
+        return
+    }
+
     try {
         const method = req.method;
         const request = req.body;
@@ -10,31 +44,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
         switch (method) {
             case 'POST':
-                const productsToAdd =
-                    gql`
-                    mutation AddProducto($nombre: String!, $precioVenta: Float!, $ean: String!, $proveedor: String, $familia: String, $precioCompra: Float, $iva: Float, $margen: Float, $promociones: [String], $cantidad: Int, $cantidadRestock: Int, $alta: Boolean) {
-                                    addProducto(nombre: $nombre, precioVenta: $precioVenta, ean: $ean, proveedor: $proveedor, familia: $familia, precioCompra: $precioCompra, iva: $iva, margen: $margen, promociones: $promociones, cantidad: $cantidad, cantidadRestock: $cantidadRestock, alta: $alta) {
-                                        message
-                                        successful
-                                    }
-                                }
-                    `;
-                useMutation(productsToAdd, {
-                    variables: {
-                        "nombre": null,
-                        "precioVenta": 10,
-                        "ean": 100,
-                        "proveedor": null,
-                        "familia": null,
-                        "precioCompra": null,
-                        "iva": null,
-                        "margen": null,
-                        "promociones": null,
-                        "cantidad": null,
-                        "cantidadRestock": null,
-                        "alta": null
-                    }
-                });
+
+
+
 
                 // if (fetchResult.data.successful) {
                 //     return res.status(200).json({ message: `Productos añadidos` });
