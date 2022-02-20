4b7e3e0162bba10893b288b862704b773484a07d
gus <gustavo_9_rj@hotmail.com>
2022-02-20 18:32:41 +0100

    modal resumen medio arreglado

--- src/components/modal/resumen/index.tsx
+++ src/components/modal/resumen/index.tsx
@@ -96,50 +96,48 @@ export const Resumen = (props: {
     return (
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} >
             <Backdrop onClick={() => { props.handleCloseResumen() }} >
-                <motion.div className="py-2 flex flex-col items-center bg-white rounded-2xl"
+                <motion.div className="flex flex-col gap-2 items-center bg-white rounded-2xl w-96 h-5/6 py-2"
                     onClick={(e) => e.stopPropagation()}
                     variants={In}
                     initial="hidden"
                     animate="visible"
                     exit="exit"
                 >
-                    <div className="w-96 max-h-96 rounded-3xl bg-white z-10 ">
-                        <h2 className="text-xl font-semibold text-center py-4">ERPWeb</h2>
-                        <div className="flex justify-evenly text-sm">
-                            <div className="text-left relative ">Cliente: {props.pagoCliente.cliente.nombre} </div>
-                            <div className="text-right relative text-black"> {fechaActual} </div>
-                        </div>
-                        <div id="receipt-content" className="text-left w-full max-h-96 text-sm p-6 ">
-                            <hr />
-                            <div className="container">
-                                <table className="w-full h-full text-xs ">
-                                    <thead className="flex w-full py-1">
-                                        <tr className="flex w-full justify-around">
-                                            <th className="w-1/4 text-left">#</th>
-                                            <th className="w-1/4 text-left">Producto</th>
-                                            <th className="w-1/4 text-center">Cantidad</th>
-                                            <th className="w-1/4 text-right">Total</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className="flex flex-col gap-2 w-full h-full bg-red-500 overflow-clip">
-                                        {
-                                            props.productosVendidos.map((prod, index) => {
-                                                if (prod.dto) {
-                                                    return <GenerarFilaProducto key={"modalRes" + prod._id} numFila={index + 1} nombreProducto={prod.nombre} cantidad={Number(prod.cantidadVendida)} precio={Number(prod.precioVenta * (1 - Number(prod.dto) / 100))} />
-                                                }
-                                                else {
-                                                    return <GenerarFilaProducto key={"modalRes" + prod._id} numFila={index + 1} nombreProducto={prod.nombre} cantidad={Number(prod.cantidadVendida)} precio={prod.precioVenta} />
-                                                }
-                                            })
-                                        }
-                                    </tbody>
-                                </table>
+                    <div className="w-full h-5/6 rounded-3xl bg-white z-10 ">
+                        <div >
+                            <h2 className="text-xl font-semibold text-center py-4">ERPWeb</h2>
+                            <div className="flex justify-evenly text-sm">
+                                <div className="text-left relative ">Cliente: {props.pagoCliente.cliente.nombre} </div>
+                                <div className="text-right relative text-black"> {fechaActual} </div>
                             </div>
+                        </div>
+                        <div id="receipt-content" className="text-left w-full h-5/6 text-sm p-4">
                             <hr />
+                            <div className="w-full h-full text-xs">
+                                <div className="flex w-full justify-around">
+                                    <p className="w-1/4 text-left font-semibold">#</p>
+                                    <p className="w-1/4 text-left font-semibold">Producto</p>
+                                    <p className="w-1/4 text-center font-semibold">Cantidad</p>
+                                    <p className="w-1/4 text-center font-semibold">Total</p>
+                                </div>
+                                <div className="flex flex-col gap-2 w-full h-full overflow-y-auto overflow-x-hidden pt-2">
+                                    {
+                                        props.productosVendidos.map((prod, index) => {
+                                            if (prod.dto) {
+                                                return <GenerarFilaProducto key={"modalRes" + prod._id} numFila={index + 1} nombreProducto={prod.nombre} cantidad={Number(prod.cantidadVendida)} precio={Number(prod.precioVenta * (1 - Number(prod.dto) / 100))} />
+                                            }
+                                            else {
+                                                return <GenerarFilaProducto key={"modalRes" + prod._id} numFila={index + 1} nombreProducto={prod.nombre} cantidad={Number(prod.cantidadVendida)} precio={prod.precioVenta} />
+                                            }
+                                        })
+                                    }
+                                </div>
+                                <hr />
+                            </div>
                         </div>
                     </div>
-                    <div className="flex justify-evenly w-full h-full">
-                        <div className="font-semibold self-center">
+                    <div className="flex justify-evenly w-full h-auto items-center">
+                        <div className="font-semibold">
                             {props.pagoCliente.tipo}
                         </div>
                         <div className="text-sm">
@@ -151,7 +149,7 @@ export const Resumen = (props: {
                             </div>
                         </div>
                     </div>
-                    <div className="px-4 pb-2 w-full flex flex-grow text-center gap-2">
+                    <div className="px-4 pb-2 w-full h-auto flex flex-grow text-center gap-2">
                         <button className="bg-red-500 hover:bg-red-600 text-white w-1/2 h-8 hover:shadow-lg rounded-lg ml-auto flex items-center justify-center" onClick={() => props.handleCloseResumen()}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
@@ -172,20 +170,20 @@ export const Resumen = (props: {
 
 const GenerarFilaProducto = (props: { numFila: number, nombreProducto: string, cantidad: number, precio: number }) => {
     return (
-        <tr className="flex w-full">
-            <td className="w-1/4 text-left">
+        <div className="flex w-full">
+            <div className="w-1/4 text-left">
                 {props.numFila}
-            </td>
-            <td className="w-1/4 text-left">
+            </div>
+            <div className="w-1/4 text-left">
                 {props.nombreProducto}
-            </td>
-            <td className="w-1/4 text-center">
+            </div>
+            <div className="w-1/4 text-center">
                 {props.cantidad}
-            </td>
-            <td className="w-1/4 text-right">
+            </div>
+            <div className="w-1/4 text-center">
                 {(props.precio * props.cantidad).toFixed(2)}€
-            </td>
-        </tr>
+            </div>
+        </div>
     );
 }
 
