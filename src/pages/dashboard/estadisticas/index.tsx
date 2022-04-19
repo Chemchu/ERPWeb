089d0fdc6b2592d9b0175e1aa5c1546113ed6312
gus <gustavo_9_rj@hotmail.com>
2022-04-19 22:12:10 +0200

    estadisticas tabs hechas

--- src/pages/dashboard/estadisticas/index.tsx
+++ src/pages/dashboard/estadisticas/index.tsx
@@ -19,7 +19,7 @@ const Stats = (props: { EmpleadoSesion: SesionEmpleado }) => {
         <Tab.Group as="div" className="flex flex-col w-full h-full pt-3 pr-2">
             <Tab.List className="flex gap-1 h-10 pr-10">
                 <Tab
-                    key={"Cierres"}
+                    key={"Ventas"}
                     className={({ selected }) =>
                         classNames(
                             'w-1/4 h-full text-sm rounded-t-2xl border-t border-x',
@@ -31,19 +31,74 @@ const Stats = (props: { EmpleadoSesion: SesionEmpleado }) => {
                     }
                 >
                     <span className='text-xl'>
-                        Estadísticas
+                        Ventas
                     </span>
                 </Tab>
+
+                <Tab
+                    key={"Productos"}
+                    className={({ selected }) =>
+                        classNames(
+                            'w-1/4 h-full text-sm rounded-t-2xl border-t border-x',
+                            'focus:outline-none ring-white ring-opacity-60',
+                            selected
+                                ? 'bg-white shadow-lg'
+                                : 'bg-gray-200 hover:bg-blue-400 hover:text-white'
+                        )
+                    }
+                >
+                    <span className='text-xl'>
+                        Productos
+                    </span>
+                </Tab>
+
+                <Tab
+                    key={"Clientes"}
+                    className={({ selected }) =>
+                        classNames(
+                            'w-1/4 h-full text-sm rounded-t-2xl border-t border-x',
+                            'focus:outline-none ring-white ring-opacity-60',
+                            selected
+                                ? 'bg-white shadow-lg'
+                                : 'bg-gray-200 hover:bg-blue-400 hover:text-white'
+                        )
+                    }
+                >
+                    <span className='text-xl'>
+                        Clientes
+                    </span>
+                </Tab>
+
             </Tab.List>
             <Tab.Panels className="flex flex-col h-90v w-full pr-2">
                 <Tab.Panel
-                    key={"Cierres"}
+                    key={"Ventas"}
                     className={classNames(
                         'pb-3 h-full w-full',
                         'focus:outline-none ring-white ring-opacity-60'
                     )}
                 >
-                    Estadísticas básicas
+                    {/* <EstadisticasPage /> */}
+                </Tab.Panel>
+
+                <Tab.Panel
+                    key={"Productos"}
+                    className={classNames(
+                        'pb-3 h-full w-full',
+                        'focus:outline-none ring-white ring-opacity-60'
+                    )}
+                >
+                    {/* <EstadisticasPage /> */}
+                </Tab.Panel>
+
+                <Tab.Panel
+                    key={"Clientes"}
+                    className={classNames(
+                        'pb-3 h-full w-full',
+                        'focus:outline-none ring-white ring-opacity-60'
+                    )}
+                >
+                    {/* <EstadisticasPage /> */}
                 </Tab.Panel>
             </Tab.Panels>
         </Tab.Group >
