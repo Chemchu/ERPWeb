cafe13e52d03c6f64eb3d621bb5d223fa17cfa5f
gus <gustavo_9_rj@hotmail.com>
2022-06-23 18:55:48 +0200

    Debug middleware

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,75 @@
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+    try {
+        console.info("-----------------------------------");
+        console.info("Entra en middleware");
+
+        if (req.nextUrl.pathname === '/') { return NextResponse.next(); }
+
+        const url = req.nextUrl.clone();
+
+        if (!req.cookies.authorization) {
+            if (req.nextUrl.pathname.includes("dashboard")) { url.pathname = "/login"; return NextResponse.rewrite(url); }
+
+            return NextResponse.next();
+        }
+
+        const authCookie = req.cookies.authorization.split(" ")[1];
+
+        console.info("Va a comprobar si el JWT está caducado");
+
+        if (IsJwtExpired(authCookie)) {
+            url.pathname = "/login";
+            return NextResponse.rewrite(url).clearCookie("authorization");
+        }
+
+
+        if (authCookie) {
+            console.info("Va a hacer fetch al servidor con el token");
+            const credentialsValidation = await fetch(`${process.env.ERPGATEWAY_URL}api/graphql`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(
+                    {
+                        query: `query ValidateJwt($jwt: String!) {
+                            validateJwt(jwt: $jwt) {
+                                validado
+                            }
+                        }`,
+                        variables: {
+                            jwt: authCookie
+                        }
+                    }
+                )
+            })
+
+            console.info("Va a recuperar el JSON del servidor")
+            const credJson = await credentialsValidation.json();
+            const authHealth = JSON.parse(credJson.data);
+
+
+            if (!authHealth.validateJwt.validado) { url.pathname = "/login"; return NextResponse.rewrite(url).clearCookie("authorization"); }
+            if (req.nextUrl.pathname === '/login') { url.pathname = "/dashboard"; return NextResponse.rewrite(url); }
+
+            return NextResponse.next();
+        }
+    }
+    catch (err) {
+        console.log(err);
+
+        const url = req.nextUrl.clone();
+        url.pathname = "/serverError"; return NextResponse.rewrite(url);
+    }
+}
+
+const IsJwtExpired = (Jwt: string): boolean => {
+    let base64Payload = Jwt.split('.')[1];
+    let payload = atob(base64Payload);
+    const exp = JSON.parse(payload.toString()).exp;
+
+    const expDate = new Date(0).setSeconds(exp);
+    return expDate <= Date.now();
+}
