e6f58f041ea30be171ed81fd0764585cbfba53d3
gus <gustavo_9_rj@hotmail.com>
2022-07-17 19:05:07 +0200

    Test custom worker

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withPWA({
   pwa: {
     dest: "public",
     register: true,
-    skipWaiting: false,
+    skipWaiting: true,
     //disable: process.env.NODE_ENV === "development",
     //disable: true
   },
